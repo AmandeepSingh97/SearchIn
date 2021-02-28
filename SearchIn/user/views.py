1b7e83552c2621811a413c582ae99fd067c690e1
Uttam Singh <uttamkhanduja@yahoo.in>
2021-02-28 23:40:07 +0530

    Added user register & user login.

--- SearchIn/user/views.py
+++ SearchIn/user/views.py
@@ -1,8 +1,10 @@
+from rest_framework.generics import RetrieveAPIView
 from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from SearchIn.user.serializers import UserRegistrationSerializer
+from SearchIn.user.serializers import UserLoginSerializer
 
 
 class UserRegistrationView(CreateAPIView):
@@ -20,5 +22,24 @@ class UserRegistrationView(CreateAPIView):
             'status code': status_code,
             'message': 'User registered  successfully',
         }
+
+        return Response(response, status=status_code)
+
+
+class UserLoginView(RetrieveAPIView):
+
+    permission_classes = (AllowAny,)
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = {
+            'success': 'True',
+            'status code': status.HTTP_200_OK,
+            'message': 'User logged in  successfully',
+            'token': serializer.data['token'],
+        }
+        status_code = status.HTTP_200_OK
 
         return Response(response, status=status_code)
